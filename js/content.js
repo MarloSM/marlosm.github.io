e28b7fbd975469bff0887d08146ae3b82bee63d3
MarloSM <marlo.tsm@gmail.com>
2016-11-30 22:30:42 +1300

    Added content.js
    Content.js allows for one file to be changed that will change throughout
    all html. Currently trying to test out different elements with css such
    as id and class.

---
+++ js/content.js
@@ -0,0 +1,16 @@
+document.write('\
+\
+<head>\
+<link rel="shortcut icon" href="images/icon.ico" />\
+<link rel="stylesheet" type="text/css" href="css/style.css">\
+<title>Marlo San Miguel</title>\
+</head>\
+\
+<body>\
+\
+<!-- Header -->\
+<header class="h1">\
+<h1>\<a href="home.html">Marlo San Miguel</a> Portfolio</h1>\
+</body>\
+\
+');
