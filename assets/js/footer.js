fc6d5abce17a05c207c28be7744a92a28349112d
MarloSM <marlo.tsm@gmail.com>
2017-01-27 11:23:31 +1300

    Footer Bug
    Footer didn't come up. Moved footer tag from html to .js to test

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,4 +1,5 @@
 document.write('\
+			<footer id="footer">\
 				<ul class="icons">\
 					<li><a class="icon fa-twitter" href="https://twitter.com/MarlosmColab?lang=en"><span class="label">Twitter</span></a></li>\
 					<li><a class="icon fa-tumblr" href="http://marlosanmiguel.tumblr.com/"><span class="label">Instagram</span></a></li>\
@@ -8,4 +9,5 @@ document.write('\
 					<li>&copy; Marlo San Miguel. All rights reserved.</li>\
 					<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>\
 				</ul>\
+			</footer>\
 ');
