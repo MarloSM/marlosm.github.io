0d13bc251e89a7620985849621297a36372dcc94
MarloSM <marlo.tsm@gmail.com>
2017-01-27 11:21:07 +1300

    Footer Update
    Placed all footer content into a .js and updated all relevant .html.
    Removed Facebook and Google+ logo

--- assets/js/content.js
+++ assets/js/content.js
@@ -9,7 +9,7 @@ document.write('\
           <li><a href="idigiation.html">I-digi-ation</a></li>\
           <li><a href="respace.html">Respace</a></li>\
           <li><a href="mug.html">Arthiritis Mug</a></li>\
-          <li><a href="leep.html">Sleep Experiment</a></li>\
+          <li><a href="sleep.html">Sleep Experiment</a></li>\
         </ul>\
       </li>\
       <li>\
